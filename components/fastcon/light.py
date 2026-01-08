2994ffbd2b90e5b3144021dea87dc0c683b75afa
oriolfda <oriolfda@gmail.com>
2026-01-08 21:47:39 +0100

    add group_id validation to light_py

--- components/fastcon/light.py
+++ components/fastcon/light.py
@@ -11,27 +11,45 @@ DEPENDENCIES = ["esp32_ble"]
 AUTO_LOAD = ["light"]
 
 CONF_CONTROLLER_ID = "controller_id"
+CONF_GROUP_ID = "group_id"  # New configuration key for groups
 
 fastcon_ns = cg.esphome_ns.namespace("fastcon")
 FastconLight = fastcon_ns.class_("FastconLight", light.LightOutput, cg.Component)
 
+# MODIFIED CONFIG SCHEMA: light_id OR group_id (not both)
 CONFIG_SCHEMA = cv.All(
     light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
         {
             cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(FastconLight),
-            cv.Required(CONF_LIGHT_ID): cv.int_range(min=1, max=255),
+            # Changed from Required to Optional for light_id
+            cv.Optional(CONF_LIGHT_ID): cv.int_range(min=1, max=255),
+            # New optional group_id parameter
+            cv.Optional(CONF_GROUP_ID): cv.int_range(min=1, max=255),
             cv.Optional(CONF_CONTROLLER_ID, default="fastcon_controller"): cv.use_id(
                 FastconController
             ),
         }
-    ).extend(cv.COMPONENT_SCHEMA)
+    ).extend(cv.COMPONENT_SCHEMA),
+    # VALIDATION: Must have either light_id OR group_id
+    cv.has_at_least_one_key(CONF_LIGHT_ID, CONF_GROUP_ID)
 )
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID], config[CONF_LIGHT_ID])
+    # Create the component variable
+    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     await cg.register_component(var, config)
     await light.register_light(var, config)
 
+    # Assign the appropriate ID (light_id or group_id)
+    if CONF_LIGHT_ID in config:
+        light_id = config[CONF_LIGHT_ID]
+        cg.add(var.set_light_id(light_id))
+    elif CONF_GROUP_ID in config:
+        group_id = config[CONF_GROUP_ID]
+        # This calls the new set_group_id() method in C++
+        cg.add(var.set_group_id(group_id))
+    
+    # Assign the controller
     controller = await cg.get_variable(config[CONF_CONTROLLER_ID])
     cg.add(var.set_controller(controller))
