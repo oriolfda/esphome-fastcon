2100d9cc5fcc2200c3bf9247ece153097d026fdf
oriolfda <oriolfda@gmail.com>
2026-01-09 15:10:06 +0100

    fix

--- components/fastcon/fastcon_controller.h
+++ components/fastcon/fastcon_controller.h
@@ -22,7 +22,7 @@ namespace esphome
             std::vector<uint8_t> get_light_data(light::LightState *state);
             std::vector<uint8_t> single_control(uint32_t addr, 
                                                 const std::vector<uint8_t> &light_data,
-                                                bool is_group)    // <-- NEW PARAMETER; 
+                                                bool is_group);    // <-- NEW PARAMETER; 
 
             void queueCommand(uint32_t light_id_, const std::vector<uint8_t> &data);
 
