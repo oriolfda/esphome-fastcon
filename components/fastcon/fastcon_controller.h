93d196cf9dd92eea307955006d8861673f9f5556
oriolfda <oriolfda@gmail.com>
2026-01-11 19:14:23 +0100

    direct ble

--- components/fastcon/fastcon_controller.h
+++ components/fastcon/fastcon_controller.h
@@ -103,7 +103,7 @@ namespace esphome
                         light::ColorMode::COLD_WARM_WHITE, // Blanc càlid/fred
                         light::ColorMode::RGB,             // Color RGB
                         light::ColorMode::RGB_WHITE,       // RGB + blanc
-                        light::ColorMode::COLOR_TEMP       // Temperatura de color
+                        light::ColorMode::RGB_COLOR_TEMP      // Temperatura de color
                     });
                     
                     traits.set_min_mireds(153);   // 6500K
