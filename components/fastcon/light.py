e681132e64d8b71750aed7081993ec5304023911
oriolfda <oriolfda@gmail.com>
2026-01-15 13:34:05 +0100

    grups2

--- components/fastcon/light.py
+++ components/fastcon/light.py
@@ -28,7 +28,13 @@ CONFIG_SCHEMA = cv.All(
             cv.Optional(CONF_LIGHT_ID): cv.int_range(min=1, max=255),
             # New optional group_id parameter
             cv.Optional(CONF_GROUP_ID): cv.int_range(min=1, max=255),
-            cv.Optional(CONF_MEMBERS): cv.ensure_list(cv.use_id(light.LightState)),
+#            cv.Optional(CONF_MEMBERS): cv.ensure_list(cv.use_id(light.LightState)),
+            cv.Optional(CONF_MEMBERS_WITH_ID): cv.ensure_list(
+                cv.Schema({
+                    cv.Required(CONF_ID): cv.use_id(light.LightState),
+                    cv.Required(CONF_LIGHT_ID): cv.int_range(min=1, max=255),
+                })
+            ),            
             cv.Optional(CONF_CONTROLLER_ID, default="fastcon_controller"): cv.use_id(FastconController),
             cv.Optional(CONF_SUPPORTS_CWWW, default=False): cv.boolean,
         }
@@ -73,27 +79,17 @@ async def to_code(config):
     # ─────────────────────────────
     # REGISTRE DE GRUPS
     # ─────────────────────────────
-    if CONF_GROUP_ID in config and CONF_MEMBERS in config:
+    if CONF_MEMBERS_WITH_ID in config:
         controller = await cg.get_variable(config[CONF_CONTROLLER_ID])
+        for m in config[CONF_MEMBERS_WITH_ID]:
+            member_state = await cg.get_variable(m[CONF_ID])
+            member_light_id = m[CONF_LIGHT_ID]
 
-        group_id = config[CONF_GROUP_ID]
-        group_light_id = 0 
-        group_light_var = var  
-
-        members = []
-        for member in config[CONF_MEMBERS]:
-            member_var = await cg.get_variable(member)
-            member_light_id = member[CONF_LIGHT_ID]  
-            members.append((member_light_id, member_var))
-
-        cg.add(
-            controller.register_group(
-                group_id,
-                group_light_id,
-                group_light_var,
-                members
-            )
-        )
+            cg.add(controller.register_group_member(
+                member_light_id,
+                config[CONF_GROUP_ID],
+                member_state
+            ))
 
 
     # Supports CWWW?
