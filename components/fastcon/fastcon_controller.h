6593a2ac13cf173672d6b1a5d9e483f47bbade64
oriolfda <oriolfda@gmail.com>
2026-01-15 09:51:28 +0100

    grups2

--- components/fastcon/fastcon_controller.h
+++ components/fastcon/fastcon_controller.h
@@ -212,7 +212,7 @@ namespace esphome
 
             std::unordered_map<uint8_t, GroupInfo> groups_;          // group_id -> GroupInfo
             std::unordered_map<light::LightState *, std::vector<uint8_t>> light_groups_; // llum -> grups pare
-        };
+        }
 
         protected:
             struct Command
