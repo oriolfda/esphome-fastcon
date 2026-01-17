625211448a8b1f1073ef0e6ce5fc417ab8db84ee
oriolfda <oriolfda@gmail.com>
2026-01-17 14:13:11 +0100

    grups2

--- components/fastcon/fastcon_light.cpp
+++ components/fastcon/fastcon_light.cpp
@@ -49,7 +49,7 @@ namespace esphome
             this->controller_->queueCommand(this->light_id_, adv_data);
 
             if (this->controller_ != nullptr) {
-              this->controller_->on_state_changed(this->light_id_, state, this->is_group);
+              this->controller_->on_state_changed(this->light_id_, state, this->is_group_);
               this->controller_->dump_groups();
             }            
 
