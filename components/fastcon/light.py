776669a0509ac48221407986687c5f147ce1dc1e
oriolfda <oriolfda@gmail.com>
2026-01-17 14:37:38 +0100

    grups2

--- components/fastcon/light.py
+++ components/fastcon/light.py
@@ -4,6 +4,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light
 from esphome.const import CONF_LIGHT_ID, CONF_OUTPUT_ID, CONF_ID
+from esphome.core import CORE
 from .fastcon_controller import FastconController
 
 # New config key to toggle RGBCW capability per-entity
@@ -19,44 +20,36 @@ CONF_MEMBERS_WITH_ID = "members_with_id"
 # Variables globals
 FASTCON_GROUPS = {}
 FASTCON_GROUP_STATES = {}
+FINALIZE_TASK_ADDED = False  # Flag per afegir la tasca només un cop
 
 fastcon_ns = cg.esphome_ns.namespace("fastcon")
 FastconLight = fastcon_ns.class_("FastconLight", light.LightOutput, cg.Component)
 
-# OPCIÓ 1: Funció finalize SIMPLIFICADA (recomanada)
-def finalize_fastcon_groups(config):
-    """Validació final que registra tots els grups"""
-    # Aquesta funció s'executa durant la validació
-    async def generate_all_groups():
-        if not FASTCON_GROUPS:
-            return
-        
-        controller_id = config.get(CONF_CONTROLLER_ID, "fastcon_controller")
-        controller = await cg.get_variable(controller_id)
-        
-        for group_id, members in FASTCON_GROUPS.items():
-            group_state = FASTCON_GROUP_STATES.get(group_id)
-            
-            for light_id, member_id in members:
-                member_state = await cg.get_variable(member_id)
-                
-                cg.add(
-                    controller.register_group_member(
-                        light_id,
-                        group_id,
-                        member_state,
-                        group_state if group_state else cg.RawExpression("nullptr")
-                    )
-                )
-    
-    # Afegir a la cua de compilació
-    cg.add_to_queue(generate_all_groups())
-    
-    # Retornar config sense canvis
-    return config
 
-# OPCIÓ 2: Usar cv.register_final_validate (més proper a l'estàndard)
-# Com que cv.register_final_validate pot ser complex, anem amb l'Opció 1
+# Funció que s'executarà al final
+async def generate_all_groups(config):
+    """Genera codi per a tots els grups"""
+    if not FASTCON_GROUPS:
+        return
+    
+    controller_id = config.get(CONF_CONTROLLER_ID, "fastcon_controller")
+    controller = await cg.get_variable(controller_id)
+    
+    for group_id, members in FASTCON_GROUPS.items():
+        group_state = FASTCON_GROUP_STATES.get(group_id)
+        
+        for light_id, member_id in members:
+            member_state = await cg.get_variable(member_id)
+            
+            cg.add(
+                controller.register_group_member(
+                    light_id,
+                    group_id,
+                    member_state,
+                    group_state if group_state else cg.RawExpression("nullptr")
+                )
+            )
+
 
 CONFIG_SCHEMA = cv.All(
     light.BRIGHTNESS_ONLY_LIGHT_SCHEMA
@@ -77,13 +70,12 @@ CONFIG_SCHEMA = cv.All(
     )
     .extend(cv.COMPONENT_SCHEMA),
     cv.has_at_least_one_key(CONF_LIGHT_ID, CONF_GROUP_ID)
-    # NOTA: finalize_fastcon_groups NO es posa aquí!
 )
 
-# En lloc d'usar cv.All, afegim el finalize DIRECTAMENT a la definició del component
-# Modifiquem async def to_code per cridar finalize_fastcon_groups
 
 async def to_code(config):
+    global FINALIZE_TASK_ADDED
+    
     light_id_value = config.get(CONF_LIGHT_ID, 0)
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID], light_id_value)  # FastconLight*
 
@@ -118,12 +110,16 @@ async def to_code(config):
                 (m[CONF_LIGHT_ID], m[CONF_ID])
             )
     
-    # 🔥 AQUESTA ÉS LA CLAU: Cridar finalize_fastcon_groups
-    # Però només un cop! Afegim un flag global
-    if not hasattr(to_code, "_finalized"):
-        # Cridar la funció de finalització
-        finalize_fastcon_groups(config)
-        to_code._finalized = True
+    # 🔥 AQUESTA ÉS LA CLAU: Afegir la tasca de finalització NOMÉS un cop
+    if not FINALIZE_TASK_ADDED:
+        # Crear una lambda que s'executi al final
+        # Ho fem amb CORE.add_job que afegeix una tasca a la cua
+        async def finalize_task():
+            await generate_all_groups(config)
+        
+        # Afegir la tasca a la cua d'ESPHome
+        CORE.add_job(finalize_task)
+        FINALIZE_TASK_ADDED = True
     
     # Supports CWWW?
     if config.get(CONF_SUPPORTS_CWWW):
